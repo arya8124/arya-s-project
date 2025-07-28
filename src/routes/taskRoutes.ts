f1970afcb3986f065a1ed5a69173bf08be84adc9
arya8124 <aryasarfare30@gmail.com>
2025-07-28 12:15:16 +0530

    Removed node_modules and .env from tracking

--- src/routes/taskRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createTask, getTasks } from '../controller/taskController';
-
-const router = Router();
-
-router.post('/tasks', createTask);
-router.get('/tasks', getTasks);
-
-export default router;
